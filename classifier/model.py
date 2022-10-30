2e55360b9c881006e77384721d8ff01ef0c551b1
owalid <othmane.el.ayadi@gmail.com>
2022-10-30 11:54:42 +0100

    clean

--- classifier/model.py
+++ classifier/model.py
@@ -1,40 +1,42 @@
 import tensorflow as tf
 import tensorflow.keras.layers as tfl
 
-def get_model(input_shape, num_classes):
-    inputs = tf.keras.Input(shape=input_shape)
+def get_model(inputs_, num_classes=3):
+  
+    inputs = tf.keras.Input(shape=(inputs_.shape[1], inputs_.shape[2]))
 
-    model = tfl.Conv1D(filters=16, kernel_size=(120,), strides=(6,1), padding="same")(inputs)
+    model = tfl.Dropout(0.05)(inputs)
+    model = tfl.Conv1D(filters=16, kernel_size=120, strides=6, padding="same")(inputs)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=32, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=32, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=80, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=80, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=80, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=80, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=80, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=80, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=80, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=80, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=80, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=80, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
-    model = tfl.Conv1D(filters=80, kernel_size=(3,), strides=(1,1), padding="same")(inputs)
+    model = tfl.Conv1D(filters=80, kernel_size=3, strides=1, padding="same")(model)
     model = tfl.ReLU()(model)
-    model = tfl.MaxPool1D(pool_size=(2,1), strides=(2,1), padding='same')(model)
+    model = tfl.MaxPool1D(pool_size=2, strides=2, padding='same')(model)
 
     model = tfl.LSTM(128, return_sequences=True)(model)
     model = tfl.ReLU()(model)
@@ -45,22 +47,24 @@ def get_model(input_shape, num_classes):
     model = tfl.LSTM(128, return_sequences=True)(model)
     model = tfl.ReLU()(model)
 
-    prediction_layer = tfl.Dense(num_classes, activation='softmax')
-    prediction_layer = tfl.Dropout(0.5)(prediction_layer)
+    outputs = tfl.Dense(num_classes, activation='softmax')(model)
+    # prediction_layer = tfl.Dropout(0.5)(prediction_layer)
 
-    outputs = prediction_layer(model)
-    model = tf.keras.Model(inputs, outputs)
-
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    # model.compile(optimizer=keras.optimizers.Adam(learning_rate=learning_rate), loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+    # model.summary()
     return model
 
 
-def run_model(x_train, x_valid, y_train, y_valid, input_shape, num_classes, batch_size, epochs):
-    model = get_model(input_shape, num_classes)
+def run_model(dataset_train, dataset_val, inputs, num_classes, epochs):
+    model = get_model(inputs, num_classes)
+    model.compile(optimizer='adam', loss="sparse_categorical_crossentropy", metrics=['accuracy'])
+    print(model.summary())
 
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy', 'mean_absolute_error', 'categorical_accuracy', 'categorical_crossentropy',])
+    history = model.fit(
+        dataset_train,
+        epochs=epochs,
+        validation_data=dataset_val,
+    )
 
-    model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(x_valid, y_valid))
-
-    return model
+    return history
